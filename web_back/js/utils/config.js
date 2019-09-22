2644e7abf4599cd2f7e4cea36fb959dd0a78326b
jiacheng-mn <1466174160@qq.com>
2019-09-22 17:51:56 +0800

    Ajax请求地址优化

---
+++ web_back/js/utils/config.js
@@ -0,0 +1,7 @@
+// Ajax请求地址优化
+var base_url = 'http://localhost:8000';
+var URL_list = {
+    user_login: base_url + '/admin/login',
+    user_logout: base_url + '/admin/logout',
+    user_getInfo: base_url + '/admin/getuser'
+}
